b4bfebfe02021723e970dcac217bbaa5bccc5aee
Jesse Hu <huyouare@users.noreply.github.com>
2023-04-27 19:07:35 -0700

    Make email and phone tools work

--- tools/email.py
+++ tools/email.py
@@ -3,14 +3,15 @@ tools/email.py
 
 This file contains tools that allow the agent to send emails and verify email addresses.
 
+VerifyEmail only works when subscribed to SendGrid Pro.
 """
 
 from core.tool import BaseTool, InputSpec, OutputSpec
-import smtplib
-import ssl
 import os
 import email
 from sendgrid import SendGridAPIClient
+from python_http_client.exceptions import HTTPError
+from sendgrid.helpers.mail import Mail, Email, To, Content
 
 class VerifyEmail(BaseTool):
     """Verifies the email is valid via SendGrid API."""
@@ -38,34 +39,35 @@ class VerifyEmail(BaseTool):
             "source": "signup"
         }
 
-        try:
-            response = self.sendgrid_api.client.validations.email.post(
-                request_body=data
-            )
-        except Exception as e:
-            print(e)
+        response = self.sendgrid_api.client.validations.email.post(
+            request_body=data
+        )
 
-            # print(response.status_code)
-            # print(response.body)
-            # print(response.headers)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
 
-            # return {
-            #     "output": response.body
-            # }
+        return {
+            "output": response.body
+        }
 
 
-class Email(BaseTool):
+class SendEmail(BaseTool):
+    """Send an email using the SendGrid API."""
     def __init__(self):
-        self.recipient: str = None
+        self.sender_email: str = None
+        self.receiver_email: str = None
         self.subject: str = None
         self.body: str = None
+        self.sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
 
     def description(self) -> str:
-        return "Sends an email."
+        return "Send an email using the SendGrid API."
     
     def input_spec(self) -> list[InputSpec]:
         return [
-            InputSpec("recipient", "The recipient of the email", "string"),
+            InputSpec("sender_email", "The sender email", "string"),
+            InputSpec("receiver_email", "The receiver email", "string"),
             InputSpec("subject", "The subject of the email", "string"),
             InputSpec("body", "The body of the email", "string"),
         ]
@@ -74,44 +76,49 @@ class Email(BaseTool):
         return []
     
     def parse_input(self, input_str: str):
-        self.recipient = input_str["recipient"]
+        self.sender_email = input_str["sender_email"]
+        self.receiver_email = input_str["receiver_email"]
         self.subject = input_str["subject"]
         self.body = input_str["body"]
 
     def run(self):
-        # Send the email
-        port = 465  # For SSL
-        smtp_server = "smtp.gmail.com"
-        sender_email = "jessehu12@gmail.com"
-        password = os.environ["EMAIL_PASSWORD"]
-        message = email.message.EmailMessage()
-        message["Subject"] = self.subject
-        message["From"] = sender_email
-        message["To"] = self.recipient
-        message.set_content(self.body)
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.send_message(message)
+        from_email = Email(self.sender_email)  # Change to your verified sender
+        to_email = To("huyouare@gmail.com")  # Always send to myself for now
+        subject = self.subject
+        content = Content("text/plain", self.body)
+        mail = Mail(from_email, to_email, subject, content)
 
-        return {
-            "output": f"Sent email to {self.recipient} with subject {self.subject} and body {self.body}",
-        }
+        # Get a JSON-ready representation of the Mail object
+        mail_json = mail.get()
+        print(f"""Sending email...
+To: {self.receiver_email}
+From {self.sender_email}
+Subject:\n{self.subject}
+Body:\n{self.body}""")
+
+        # Send an HTTP POST request to /mail/send
+        try:
+            response = self.sg.client.mail.send.post(request_body=mail_json)
+            return { "output": response.body }
+        except HTTPError as e:
+            print("Sendgrid error:", e.to_dict)
+            return { "output": str(e.to_dict) }
+
 
 if __name__ == "__main__":
-    # Test verifying email
-    verify_email = VerifyEmail()
-    verify_email.parse_input({
-        "email": "jessehu12@gmail.com",
-    })
-    verify_email.run()
-
+    # # Test verifying email
+    # verify_email = VerifyEmail()
+    # verify_email.parse_input({
+    #     "email": "jessehu12@gmail.com",
+    # })
+    # verify_email.run()
 
     # Test sending email
-    email = Email()
-    email.parse_input({
-        "recipient": "huyouare@gmail.com",
-        "subject": "Test",
+    send_email = SendEmail()
+    send_email.parse_input({
+        "sender_email": "jessehu12@gmail.com",
+        "receiver_email": "huyouare@gmail.com",
+        "subject": "Test email",
         "body": "This is a test email.",
     })
-    email.run()
+    send_email.run()
--- tools/phone_call.py
+++ tools/phone_call.py
@@ -6,11 +6,21 @@ A tool that allows the agent to make a phone call.
 """
 
 from core.tool import BaseTool, InputSpec, OutputSpec
+import os
+import vocode
+from vocode.streaming.telephony.hosted.outbound_call import OutboundCall
+from vocode.streaming.models.telephony import CallEntity
+from vocode.streaming.models.agent import ChatGPTAgentConfig
+from vocode.streaming.models.message import BaseMessage
+from vocode.streaming.models.telephony import TwilioConfig
+
 
 class PhoneCall(BaseTool):
     def __init__(self):
-        self.phone_number: str = None
-        self.context: str = None
+        vocode.api_key = os.getenv("VOCODE_API_KEY")
+        self.phone_number: str = os.getenv("RECIPIENT_PHONE_NUMBER")
+        self.prompt: str = None
+        self.caller_phone_number: str = os.getenv("CALLER_PHONE_NUMBER")
 
     def description(self) -> str:
         return "Makes a phone call."
@@ -18,7 +28,7 @@ class PhoneCall(BaseTool):
     def input_spec(self) -> list[InputSpec]:
         return [
             InputSpec("phone_number", "The phone number to call", "string"),
-            InputSpec("context", "The context of the phone call", "string"),
+            InputSpec("prompt", "The prompt given to the LLM making the phone call", "string"),
         ]
 
     def output_spec(self) -> list[OutputSpec]:
@@ -26,9 +36,33 @@ class PhoneCall(BaseTool):
 
     def parse_input(self, input_str: str):
         self.phone_number = input_str["phone_number"]
-        self.context = input_str["context"]
+        self.prompt = input_str["prompt"]
 
     def run(self):
+        call = OutboundCall(
+            recipient=CallEntity(
+                phone_number=self.phone_number,
+            ),
+            caller=CallEntity(
+                phone_number=self.caller_phone_number,
+            ),
+            agent_config=ChatGPTAgentConfig(
+                initial_message=BaseMessage(text="Hello!"),
+                prompt_preamble=self.prompt,
+            ),
+            twilio_config=TwilioConfig(
+                account_sid=os.getenv("TWILIO_ACCOUNT_SID"),
+                auth_token=os.getenv("TWILIO_AUTH_TOKEN"),
+            )
+        )
+        call.start()
+
         return {
-            "output": f"Calling {self.phone_number} with context {self.context}",
-        }
+                "output": f"Calling {self.phone_number} with prompt {self.prompt}",
+            }
+
+
+if __name__ == "__main__":
+    tool = PhoneCall()
+    tool.prompt = "You are a person named Jesse. You are calling Avis to request a refund for etoll charge for rental agreement number 12345, date of rental 4/20/23, and contact information blahblah@gmail.com and phone number 9995556666."
+    tool.run()
