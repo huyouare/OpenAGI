4f7b6179c88ac851d2fff8efbbeb8b4243bb1fe7
Jesse Hu <huyouare@users.noreply.github.com>
2023-07-26 14:27:00 -0700

    Update function call prompt

--- core/agent.py
+++ core/agent.py
@@ -4,10 +4,22 @@ core/agent.py
 This file contains the agent implementation.
 """
 
+import ast
 import json
 from models import llm
 from textwrap import dedent
-from tools import browse, code, datetime_tool, email, location, phone_call, search, summarize, terminal, user_input
+from tools import (
+    browse,
+    code,
+    datetime_tool,
+    email,
+    location,
+    phone_call,
+    search,
+    summarize,
+    terminal,
+    user_input,
+)
 from dataviz.visualizer.visualizer import Visualizer
 
 
@@ -28,7 +40,7 @@ If there is a task you cannot currently do, you can prompt the user for help via
 
 You should minimize the number of steps you take and tools used, while maximizing the quality of the results.
 
-Now, generate a plan to achieve the objective."""
+Now, generate a very concise plan (no complete sentences or explanation) to achieve the objective."""
 
 ORIG_TOOL_PROMPT = """
 If provided, the output (or error) from the tool used is provided below:
@@ -47,10 +59,7 @@ If you are stuck, ask the UserInput for clarification.
 At the end of your response, output the action in the following JSON format.
 Do NOT include any other text in your ACTION.
 
-{{
-    "action": "tool_name",
-    "input": {{ "input_name": "input_value" }}
-}}
+{{ "action": "tool_name", "input": {{ "input_name": "input_value" }} }}
 
 REFLECT:"""
 
@@ -60,11 +69,16 @@ If provided, the output (or error) from the tool used is provided below:
 
 Now, continue working towards the objective.
 
+As a reminder, here are the tools you can use:
+{tool_prompt}
+
 If the you feel that the task is complete, use the UserInput tool to confirm.
 If you are stuck, ask the UserInput for clarification.
 
-Do NOT return anything other than your function call.
+ALWAYS call a function.
+Do NOT return any text other than your function call.
 """
+
 
 class AgentState:
     NOT_STARTED = 0
@@ -110,10 +124,12 @@ class Agent:
         self.tool_prompt = "\n".join(tool_strings)
 
         # Ask the user for what the agent objective is
-        self.objective = input("""\033[1m
+        self.objective = input(
+            """\033[1m
 Hi, my name is DAN. I am a general agent that can Do Anything Now.
-What can I do for you today?\n\n\033[0m> """)
-                               
+What can I do for you today?\n\n\033[0m> """
+        )
+
         self.visualizer.add_new_stage(title="Objective", content=self.objective)
 
         # Generate a chat completion
@@ -121,7 +137,7 @@ What can I do for you today?\n\n\033[0m> """)
             INTRO_PROMPT.format(objective=self.objective, tool_prompt=self.tool_prompt)
         )
         print("\033[95m" + prompt + "\033[0m")
-        viz_id =  self.visualizer.add_new_stage(title="Planning", content="Thinking...")
+        viz_id = self.visualizer.add_new_stage(title="Planning", content="Thinking...")
         response = self.model.generate_chat_completion(prompt)
         print("\033[94m" + response)
         self.visualizer.amend_stage(stage_id=viz_id, content=response)
@@ -136,24 +152,44 @@ What can I do for you today?\n\n\033[0m> """)
         while self.state == AgentState.RUNNING:
             # Generate a chat completion
             prompt = dedent(
-                FUNC_CALL_PROMPT.format(tool_output=tool_output) if USE_FUNCTION_CALLS else ORIG_TOOL_PROMPT.format(tool_output=tool_output)
+                FUNC_CALL_PROMPT.format(
+                    tool_output=tool_output, tool_prompt=self.tool_prompt
+                )
+                if USE_FUNCTION_CALLS
+                else ORIG_TOOL_PROMPT.format(tool_output=tool_output)
             )
             print("\033[95m" + prompt + "\033[0m")
-            viz_id = self.visualizer.add_new_stage(title="Reflection", content="Thinking...")
+            viz_id = self.visualizer.add_new_stage(
+                title="Reflection", content="Thinking..."
+            )
 
             if USE_FUNCTION_CALLS:
                 functions = [tool.json_openai() for tool in self.tools]
                 print("Functions:", functions)
-                message = self.model.generate_chat_completion_with_functions(prompt, self.messages + self.assistant_messages, functions)
-                print("Message:", message)
-                response = None
-                if "function_call" in message:
-                    print("\033[94m" + "Function call: " + str(message.function_call) + "\033[0m")
-                    response = message.function_call
-                else:
-                    print("\033[94m" + str(message.content) + "\033[0m")
-                    response = json.loads(message.content)
-                
+                message = self.model.generate_chat_completion_with_functions(
+                    prompt, self.messages + self.assistant_messages, functions
+                )
+
+                try:
+                    print("Message:", message)
+                    response = None
+                    if "function_call" in message:
+                        print(
+                            "\033[94m"
+                            + "Function call: "
+                            + str(message.function_call)
+                            + "\033[0m"
+                        )
+                        response = message.function_call
+                    else:
+                        print("\033[94m" + str(message.content) + "\033[0m")
+                        response = ast.literal_eval(message.content)
+                        # Strip out "function."
+                        if "function." in response["name"]:
+                            response["name"] = response["name"].split(".")[1]
+                except Exception as error:
+                    print(f"Error parsing message.content: {error}")
+
                 # Use the function call to get the tool by name
                 function_name = response["name"]
                 tool = None
@@ -161,7 +197,7 @@ What can I do for you today?\n\n\033[0m> """)
                     if type(t).__name__ == function_name:
                         tool = t
                         break
-                
+
                 print("RUNNING TOOL:", tool)
 
                 # Set the tool's input
@@ -175,13 +211,19 @@ What can I do for you today?\n\n\033[0m> """)
                     print("Error: " + tool_output)
 
             else:
-                response = self.model.generate_chat_completion(prompt, self.messages + self.assistant_messages)
+                response = self.model.generate_chat_completion(
+                    prompt, self.messages + self.assistant_messages
+                )
                 print("\033[94m" + response + "\033[0m")
 
                 # Parse the text response after "ACTION" as JSON.
                 action_str = response.split("ACTION:")[1]
-                viz_id = self.visualizer.add_new_stage(title="Action", content=action_str)
-                self.visualizer.amend_stage(stage_id=viz_id, content=response.split("ACTION:")[0])
+                viz_id = self.visualizer.add_new_stage(
+                    title="Action", content=action_str
+                )
+                self.visualizer.amend_stage(
+                    stage_id=viz_id, content=response.split("ACTION:")[0]
+                )
 
                 print("Action str:", action_str)
 
@@ -201,7 +243,7 @@ What can I do for you today?\n\n\033[0m> """)
                     if type(t).__name__ == output["action"]:
                         tool = t
                         break
-                
+
                 # Set the tool's input
                 try:
                     tool.parse_input(output["input"])
@@ -211,18 +253,22 @@ What can I do for you today?\n\n\033[0m> """)
                     tool_output = str(error)
                     print("Error: " + tool_output)
 
-                viz_id = self.visualizer.add_new_stage(title=output["action"], content=str(tool_output))
+                viz_id = self.visualizer.add_new_stage(
+                    title=output["action"], content=str(tool_output)
+                )
 
             if len(self.assistant_messages) > 10:
                 self.assistant_messages.pop(0)
                 self.assistant_messages.pop(0)
             self.assistant_messages.append({"role": "user", "content": prompt})
-            self.assistant_messages.append({"role": "assistant", "content": str(response)})
+            self.assistant_messages.append(
+                {"role": "assistant", "content": str(response)}
+            )
 
             # TODO: Add the tool input and output to messages
             # if output["action"] == "UserInput":
             #     self.assistant_messages.append({"role": "user", "content": str(tool_output)})
-            
+
             # # Fake the tool being run by getting user input
             # tool_output = input("Please enter the output from the tool:\n")
 
